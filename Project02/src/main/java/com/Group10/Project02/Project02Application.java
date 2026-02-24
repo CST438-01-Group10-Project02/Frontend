de10a8e649b6ec164f9b81124367b2541755502b
Peter Gloag <petergloag@gmail.com>
2026-02-24 15:02:19 -0800

    Created a super simple hello world program using springboot.

--- Project02/src/main/java/com/Group10/Project02/Project02Application.java
+++ Project02/src/main/java/com/Group10/Project02/Project02Application.java
@@ -2,12 +2,19 @@ package com.Group10.Project02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Project02Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project02Application.class, args);
 	}
-
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
